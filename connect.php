0d12495843821dc7a2ef3b885c6239fc5885b123
zaydin2000 <zaydin2000@gmail.com>
2022-04-19 18:03:43 +0300

    Odev

---
+++ connect.php
@@ -0,0 +1,9 @@
+﻿<?php
+	
+	$link = mysqli_connect("localhost", "root")or die("Mysql Bağlantısı kurulamadı.");
+
+	mysqli_select_db($link, "proje") or die("Veritabanına bağlanılamadı.");
+
+	mysqli_query($link,"SET NAMES UTF8");
+
+?>
